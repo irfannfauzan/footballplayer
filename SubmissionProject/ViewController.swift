6d8750da060defa794ab1dae45d57bc208a28880
Irfan Fauzan <irfannfauzan@irfans-macbook-air.local>
2020-11-21 20:48:18 +0700

    Initial Commit

---
+++ SubmissionProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SubmissionProject
+//
+//  Created by Irfan Fauzan R on 21/11/20.
+//  Copyright © 2020 Irfan Fauzan R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
